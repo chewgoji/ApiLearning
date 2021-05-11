4b027016beb18ce79cd44b7380f67849a6beb5f6
WilliamChu <willi0955@gmail.com>
2021-05-11 14:07:25 +0800

    first commit

---
+++ Controllers/MaskController.cs
@@ -0,0 +1,48 @@
+﻿using ApiLearning.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiLearning.Controllers
+{
+    //[Route("[controller]")]
+    [ApiController]
+    public class MaskController : ControllerBase
+    {
+        private readonly MaskService _maskService;
+
+        public MaskController(MaskService maskService)
+        {
+            _maskService = maskService;
+        }
+
+        /*public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var maskCount = await _maskService.GetMaskInfo();
+                return Ok(maskCount);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Problem(ex.Message);
+            }
+        }*/
+
+        [Route("[controller]/{address}")]
+        public async Task<IActionResult> Search(string address)
+        {
+            var maskCount = await _maskService.GetMaskInfo();
+
+            if (!string.IsNullOrEmpty(address))
+            {
+                maskCount = maskCount.Where(x => x.醫事機構地址.Contains(address));
+            }
+
+            return Ok(maskCount);
+        }
+    }
+}
---
+++ Service/MaskService.cs
@@ -0,0 +1,32 @@
+﻿using ApiLearning.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiLearning.Service
+{
+    public class MaskService
+    {
+        public HttpClient Client { get; }
+
+        public MaskService(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://quality.data.gov.tw/");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("User-Agent", "QueryMask Sample");
+            Client = client;
+        }
+
+        public async Task<IEnumerable<MaskInfo>> GetMaskInfo()
+        {
+            var response = await Client.GetAsync("dq_download_json.php?nid=116285&md5_url=53a72b2dcfdd9ecae43afda4b86089be");
+            response.EnsureSuccessStatusCode();
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            var result = await JsonSerializer.DeserializeAsync<IEnumerable<MaskInfo>>(responseStream);
+            return result;
+        }
+    }
+}
